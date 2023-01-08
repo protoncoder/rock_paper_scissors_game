ea9a7e4c5f7a8e9e23a792a9d9ac8756ee07c8d3
protoncoder <37478616+protoncoder@users.noreply.github.com>
2023-01-08 20:25:43 +0200

    finish rock_paper_scissors game

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
+let playerScore = 0;
+let compScore = 0;
+
 //will randomly return rock, paper, scissors as computer
-function getComputerChoice () {
+let getComputerChoice = () => {
   //generate random number between 0 and 1
   let random = Math.random();
   if (random <= 0.66 && random >= 0.34) {
@@ -15,6 +18,78 @@ function getComputerChoice () {
     return "scissors";
   }
 }
-getComputerChoice();
+//getComputerChoice();
 
+//func to play a single round of Rock Scissors Paper
+let playRound = (playerSelection, computerSelection) => {
+  //console.log('1 ', playerSelection, '2 ', computerSelection);
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    return "It's a tie!";
+  }
+  else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "It's a tie!";
+  }
+  else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "It's a tie!";
+  }
+  else if (playerSelection === "rock" && computerSelection === "paper") {
+    compScore++;
+    return "You lost! Paper beats Rock!";
+  }
+  else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    return "You won! Rock beats Scissors";
+  }
+  else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    return "You won! Scissors beats Paper";
+  }
+  else if (playerSelection === "scissors" && computerSelection === "rock") {
+    compScore++;
+    return "You lost! Rock beats Scissors!";
+  }
+  else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    return "You won! Paper beats Rock!";
+  }
+  else if (playerSelection === "paper" && computerSelection === "scissors") {
+    compScore++;
+    return "You lost! Scissors beats Paper!";
+  }
+}
 
+//let playerSelection;
+let computerSelection = getComputerChoice();
+//console.log(`${playerSelection},${computerSelection}`);
+//console.log(playRound(playerSelection, computerSelection));
+
+let game = () => {
+  for (i = 0; i < 5; i++) {
+    let playerSelection = prompt("Make your choice: ", "Rock, Paper, Scissors").toLowerCase();
+    console.log(playRound(playerSelection, getComputerChoice()));
+  }
+  if (playerScore > compScore) {
+    console.log("");
+    console.log("**********************");
+    console.log("* You won! Congrats! *");
+    console.log("**********************");
+  }
+  else if (compScore > playerScore) {
+    console.log("");
+    console.log("*****************************");
+    console.log("* You lost! Condolescenses! *");
+    console.log("*****************************");
+  }
+  else if (playerScore === compScore) {
+    console.log("");
+    console.log("***************");
+    console.log("* It's a tie! *");
+    console.log("***************");
+  }
+}
+
+game();
+console.log("");
+console.log("playerScore: ", playerScore);
+console.log("compScore: ", compScore);
+console.log("");
